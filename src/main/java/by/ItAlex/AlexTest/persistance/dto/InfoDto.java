72f75a5bfd0577a4ed713db03e7c5cbbc2b6fb1a
bulbash <zverovik@yahoo.com>
2021-03-16 00:11:04 +0300

    Вернул утилитные методы в дто

--- src/main/java/by/ItAlex/AlexTest/persistance/dto/InfoDto.java
+++ src/main/java/by/ItAlex/AlexTest/persistance/dto/InfoDto.java
@@ -1,5 +1,6 @@
 package by.ItAlex.AlexTest.persistance.dto;
 
+import by.ItAlex.AlexTest.persistance.model.Info;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,5 +15,28 @@ public class InfoDto {
     private Long price;
 
     private ToolDto toolDto;
+
+    InfoDto modelToDto(Info info) {
+        if (info != null) {
+            setId(info.getId());
+            setDate(info.getDate());
+            setPrice(info.getPrice());
+            setToolDto(new ToolDto().modelToDto(info.getTool()));
+        }
+
+        return this;
+    }
+
+    Info dtoToModel() {
+
+        Info info = new Info();
+
+        info.setId(id);
+        info.setDate(date);
+        info.setPrice(price);
+        info.setTool(toolDto.dtoToModel());
+
+        return info;
+    }
 
 }
--- src/main/java/by/ItAlex/AlexTest/persistance/dto/ToolDto.java
+++ src/main/java/by/ItAlex/AlexTest/persistance/dto/ToolDto.java
@@ -1,9 +1,11 @@
 package by.ItAlex.AlexTest.persistance.dto;
 
+import by.ItAlex.AlexTest.persistance.model.Tool;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
@@ -11,6 +13,35 @@ public class ToolDto {
 
     private Long id;
     private String name;
-    private List<InfoDto> infoDtos;
+    private List<InfoDto> infosDto;
+
+    ToolDto modelToDto(Tool tool) {
+        if (tool != null) {
+            setId(tool.getId());
+            setName(tool.getName());
+            setInfosDto(tool.getInfos()
+                    .stream()
+                    .map(t -> new InfoDto().modelToDto(t))
+                    .collect(Collectors.toList())
+            );
+        }
+
+        return this;
+    }
+
+    Tool dtoToModel() {
+        Tool tool = new Tool();
+
+        tool.setId(id);
+        tool.setName(name);
+        tool.setInfos(
+                getInfosDto()
+                        .stream()
+                        .map(InfoDto::dtoToModel)
+                        .collect(Collectors.toList())
+        );
+
+        return tool;
+    }
 
 }
